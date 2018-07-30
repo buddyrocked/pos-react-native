64685bf05b5734fc3a870810e6ba6bc8a8ff4f09
Budi Hariyana <budihariyana@gmail.com>
2018-07-30 19:11:20 +0700

    reducer

--- src/reducers/reducer_create_cart.js
+++ src/reducers/reducer_create_cart.js
@@ -11,7 +11,14 @@ const defaultState = {
 export default function(state = {}, action){
   switch(action.type){
   case CREATE_CART:
-    return Object.assign({}, state, action.payload.data);
+    //return Object.assign({}, state, action.payload.data);
+    return Object.assign({}, state, {
+      items      : [],
+      count      : 100,
+      total      : 0,
+      total_text : '',
+      terbilang  : '',
+    });
   default:
     return state;
   }
