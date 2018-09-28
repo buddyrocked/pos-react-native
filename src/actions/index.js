e93dcfdad1cd03f145e03f9d965615541a29f4e2
Budi Hariyana <budihariyana@gmail.com>
2018-09-28 11:00:24 +0700

    use redux thunk to getstate on action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,9 +27,6 @@ export const FETCH_STORE      = 'fetch_store';
 
 const ROOT_URL = `http://192.168.20.250/point-of-sales/backend/web/v1/`;
 //const ROOT_URL = `http://192.168.43.216/delucent/backend/web/v1/`;
-//const ACCESS_TOKEN = deviceStorage.getStorage('user_token');
-const ACCESS_TOKEN = 'oSIuEDLQ9Qg0j32Acp69_ofAzZtACq2z';
-//const API_KEY = `?access-token=${ACCESS_TOKEN}`;
 
 export function fetchHome() {
   const request = {
@@ -43,22 +40,24 @@ export function fetchHome() {
   }
 }
 
-export const login = (values, callback) => {
-  const request = axios.post(`${ROOT_URL}auth/login`, values);
+export const login = (values, callback) => async (dispatch, getState) => {
+  try{
+    let request = await axios.post(`${ROOT_URL}auth/login`, values);
 
-  axios.post(`${ROOT_URL}auth/login`, values)
-  .then((responseJson) => {
-    if(responseJson.data.token != '') {
-      callback();
-    } else {
-      Alert.alert(responseJson.data.message);
-      callback();
-    }
-  });
+    axios.post(`${ROOT_URL}auth/login`, values)
+    .then((responseJson) => {
+      if(responseJson.data.token == '') {
+        Alert.alert(responseJson.data.message);
+        callback();
+      }
+    });
 
-  return {
-    type: LOGIN,
-    payload: request
+    dispatch({
+      type: LOGIN,
+      payload: request
+    });
+  }catch(e){
+    console.log(e)
   }
 }
 
@@ -77,147 +76,165 @@ export const fetchProducts = (page = 1, term= 'pink') => async (dispatch, getSta
 
     let request = await axios.get(`${ROOT_URL}prices${API_KEY}&expand=product&page=${page}&term=${term}`);
     dispatch({ type: FETCH_PRODUCTS, payload: request });
-
-    //return { type: FETCH_PRODUCTS, payload: request };
   }catch(e){
     console.log(e)
   }
-
 }
 
-export function fetchProduct(id) {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const fetchProduct = (id) => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}products/${id}${API_KEY}`);
-  return {
-    type: FETCH_PRODUCT,
-    payload: request
+    let request = await axios.get(`${ROOT_URL}products/${id}${API_KEY}`);
+    dispatch({type: FETCH_PRODUCT, payload: request});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function fetchCarts() {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const fetchCarts = () => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}carts${API_KEY}`);
-  return {
-    type: FETCH_CARTS,
-    payload: request
-  };
-}
-
-export function createCart(values, callback){
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
-
-  const request = axios.post(`${ROOT_URL}carts${API_KEY}`, values)
-  .then((responseJson) => {
-    Alert.alert(responseJson.data.message);
-    callback();
-  });
-
-  return {
-    type: CREATE_CART,
-    payload: request
+    let request = await axios.get(`${ROOT_URL}carts${API_KEY}`);
+    dispatch({type: FETCH_CARTS, payload: request});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function submitCart(values, callback){
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const createCart = (values, callback) => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.post(`${ROOT_URL}carts/submit${API_KEY}`, values)
-  .then((responseJson) => {
-    Alert.alert(responseJson.data.message);
-    callback();
-  });
-
-  return {
-    type: SUBMIT_CART,
-    payload: request
+    let request = await axios.post(`${ROOT_URL}carts${API_KEY}`, values)
+    .then((responseJson) => {
+      Alert.alert(responseJson.data.message);
+      callback();
+    });
+    dispatch({type: CREATE_CART, payload: request});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function updateCart(id, values, callback){
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const submitCart = (values, callback) =>  async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.put(`${ROOT_URL}carts/${id}${API_KEY}`, values)
-  .then((responseJson) => {
-    Alert.alert(responseJson.data.message);
-    callback();
-  });
-
-  return {
-    type: UPDATE_CART,
-    payload: request
+    let request = await axios.post(`${ROOT_URL}carts/submit${API_KEY}`, values)
+    .then((responseJson) => {
+      Alert.alert(responseJson.data.message);
+      callback();
+    });
+    dispatch({type: SUBMIT_CART,payload: request});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function fetchCart() {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const updateCart = (id, values, callback) =>  async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}carts${API_KEY}`);
-  return {
-    type: FETCH_CART,
-    payload: request
-  };
+    let request = await axios.put(`${ROOT_URL}carts/${id}${API_KEY}`, values)
+    .then((responseJson) => {
+      Alert.alert(responseJson.data.message);
+      callback();
+    });
+    dispatch({type: UPDATE_CART, payload: request});
+  }catch(e){
+    console.log(e)
+  }
 }
 
-export function getCart() {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const fetchCart = () =>  async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}carts${API_KEY}`);
-  return {
-    type: GET_CART,
-    payload: request
-  };
+    let request = await axios.get(`${ROOT_URL}carts${API_KEY}`);
+    dispatch({type: FETCH_CART, payload: request});
+  }catch(e){
+    console.log(e)
+  }
 }
 
-export function deleteCart(id, callback) {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const getCart = () => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.delete(`${ROOT_URL}carts/${id}${API_KEY}`)
+    let request = await axios.get(`${ROOT_URL}carts${API_KEY}`);
+    dispatch({type: GET_CART, payload: request});
+  }catch(e){
+    console.log(e)
+  }
+}
+
+export const deleteCart = (id, callback) => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
+
+    let request = await axios.delete(`${ROOT_URL}carts/${id}${API_KEY}`)
     .then(() => callback());
-  return {
-    type: DELETE_CART,
-    payload: id
+    dispatch({type: DELETE_CART, payload: id});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function clearCart(callback) {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const clearCart = (callback) => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.delete(`${ROOT_URL}carts/clear-cart${API_KEY}`)
+    let request = await axios.delete(`${ROOT_URL}carts/clear-cart${API_KEY}`)
     .then(() => callback());
-  return {
-    type: CLEAR_CART,
-    payload: request
+    dispatch({type: CLEAR_CART,payload: request});
+  }catch(e){
+    console.log(e)
   }
 }
 
-export function reportIndex() {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const reportIndex = () => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}reports${API_KEY}`);
-  return {
-    type: REPORT_INDEX,
-    payload: request
-  };
+    let request = await axios.get(`${ROOT_URL}reports${API_KEY}`);
+    dispatch({type: REPORT_INDEX, payload: request});
+  }catch(e){
+    console.log(e)
+  }
 }
 
-export function fetchStores() {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const fetchStores = () => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}stores${API_KEY}`);
-  return {
-    type: FETCH_STORES,
-    payload: request
-  };
+    let request = await axios.get(`${ROOT_URL}stores${API_KEY}`);
+    dispatch({type: FETCH_STORES, payload: request});
+  }catch(e){
+    console.log(e)
+  }
 }
 
-export function fetchStore(id) {
-  const API_KEY = `?access-token=${ACCESS_TOKEN}`;
+export const fetchStore = (id) => async (dispatch, getState) => {
+  try {
+    const TOKEN = getState().auth.token;
+    const API_KEY = `?access-token=${TOKEN}`;
 
-  const request = axios.get(`${ROOT_URL}stores/${id}${API_KEY}`);
-  return {
-    type: FETCH_STORE,
-    payload: request
-  };
+    let request = await axios.get(`${ROOT_URL}stores/${id}${API_KEY}`);
+    dispatch({type: FETCH_STORE, payload: request});
+  }catch(e){
+    console.log(e)
+  }
 }
